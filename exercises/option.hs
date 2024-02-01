6424c01e693133c84c7a0c317a1060bd241fc307
Ioan Latrovaliev <latrovalievioan@gmail.com>
2024-02-01 09:38:47 +0200

    example for apply with option data

--- exercises/option.hs
+++ exercises/option.hs
@@ -9,14 +9,16 @@ safeDiv :: Int -> Int -> Option Int
 safeDiv _ 0 = None
 safeDiv x y = Some $ div x y
 
+
 instance Functor Option where
   fmap :: (a -> b) -> Option a -> Option b
   fmap fn (Some x) = Some $ fn x
   fmap _ None = None
-
+-- fmap usage examples:
 safeDivThenIncrement :: Int -> Int -> Option Int
 safeDivThenIncrement _ 0 = None
 safeDivThenIncrement x y = increment <$> safeDiv x y
+
 
 instance Applicative Option where
   pure :: a -> Option a
@@ -25,3 +27,7 @@ instance Applicative Option where
   (<*>) :: Option (a -> b) -> Option a -> Option b
   (<*>) (Some fn) (Some x) = Some (fn x)
   (<*>) _ _ = None
+
+-- apply usage examples:
+sumTwoOptions :: Option Int -> Option Int -> Option Int
+sumTwoOptions ox oy = ((+) <$> ox) <*> oy
